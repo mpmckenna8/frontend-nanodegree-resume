7cb21d73c6e24863e3bbdc47ebfb212e0d7fbe75
Matthew McKenna <mpmckenna8@me.com>
2015-04-14 07:51:51 -0700

    adding some jam to the page

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,10 +2,13 @@
 var bio = {
   name: "Matthew Philip McKenna",
   role: "Digital Cartographer",
-  contact:"mpmckenna8@me.com",
-  pic:"mee.jpg",
-  welcome:"Get in touch if you have any questions my resume doesn't answer",
-  skills:["JavaScript", "HTML", "GIS"]
+  email:"mpmckenna8@me.com",
+  pic:"images/mee.jpg",
+  welcome:"Get in touch if you have any questions my resume doesn't answer!",
+  skills:["JavaScript", "HTML", "GIS", "Networks", "Cartography"],
+  mobile:"Please contact by e-mail to obtain mobile #",
+  contact:"email",
+  twitter:"mpmckenna8"
 }
 
 var formatName = HTMLheaderName.replace("%data%", bio.name);
@@ -17,7 +20,39 @@ var formatRole = HTMLheaderRole.replace("%data%", bio.role);
 $("#header").append(formatRole);
 
 
-var formatCon = HTMLContactGeneric.replace("%data%", bio.contact)
+var formatCon = HTMLcontactGeneric.replace("%data%", bio.contact).replace("%contact%", "Best to contact by:");
+
+var forMob = HTMLmobile.replace("%data%", bio.mobile);
+
+var formatemail = HTMLemail.replace("%data%", bio.email);
+
+var formtwit = HTMLtwitter.replace("%data%", bio.twitter);
+
+
+var formskills = [];
+for(i in bio.skills){
+  console.log(i)
+  formskills.push(HTMLskills.replace("%data%", bio.skills[i]))
+
+}
+
+var formskillsStart = HTMLskillsStart;
+
+$("#header").append(formatCon).append(formatemail).append(forMob).append(formtwit).append(formskillsStart)//.append(formskills);
+
+console.log(formskills)
+$("#skills").append(formskills)
+
+var formpic = HTMLbioPic.replace("%data%", bio.pic);
+
+var formwel = HTMLwelcomeMsg.replace("%data%", bio.welcome)
+
+$("#header").append(formpic).append(formwel)
+
+
+
+
+
 
 var awesomeThoughts = "I am Matt, and I am awesome.";
 
