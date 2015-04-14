20718b0dc8ba94a625d778f88c2cfd0df6441c6e
Matthew McKenna <mpmckenna@gmail.com>
2015-04-13 17:46:36 -0700

    adding some files and stuff

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,18 +1,23 @@
-var name = "Matthew Philip McKenna";
 
+var bio = {
+  name: "Matthew Philip McKenna",
+  role: "Digital Cartographer",
+  contact:"mpmckenna8@me.com",
+  pic:"mee.jpg",
+  welcome:"Get in touch if you have any questions my resume doesn't answer",
+  skills:["JavaScript", "HTML", "GIS"]
+}
 
-
-var formatName = HTMLheaderName.replace("%data%", name);
-
+var formatName = HTMLheaderName.replace("%data%", bio.name);
 console.log(formatName);
 $("#header").append([formatName]);
 
-var role = "Digital Cartographer";
-
-var formatRole = HTMLheaderRole.replace("%data%", role);
+var formatRole = HTMLheaderRole.replace("%data%", bio.role);
 
 $("#header").append(formatRole);
 
+
+var formatCon = HTMLContactGeneric.replace("%data%", bio.contact)
 
 var awesomeThoughts = "I am Matt, and I am awesome.";
 
