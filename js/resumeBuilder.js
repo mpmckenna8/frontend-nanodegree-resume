12c0d8dd3da0521be5d866b8d54a940a8797f089
mpmckenna8 <mpmckenna8@me.com>
2015-05-04 14:19:20 -0700

    git it a bit more working with a couple of my online courses.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,5 @@
-// Datas I'm going to have multiple so the helper s
+// Datas I'm going to have multiple so the helper stuff below
+// there's a bio, education, work and projects objects
 var bio = {
   name: "Matthew McKenna",
   role: "Digital Cartographer",
@@ -17,31 +18,51 @@ var bio = {
 
 
 // work data basically has a jobs array
-var work = {jobs:[]
+var work = {jobs:[],
+  init:function(){
+    worknow = {};
+    worknow.position = "Volunteer Supervisor";
+
+    worknow.employer = "San Francisco Bicycle Coalition";
+
+    worknow.location = "San Francisco";
+
+    worknow.years = '3.5 years';
+
+    worknow.description = "Oversee field operations for the bicycle valet service while overseeing volunteers and coordinating operations at a wide variety of sites.";
+
+    console.table(worknow)
+
+
+    work.jobs.push(worknow);
+    var wornow = {};
+    wornow.position = "Barista";
+    wornow.employer = "It's a Grind";
+    wornow.location = "San Francisco";
+    wornow.years = "5 years";
+    wornow.description = "Provide customer service in a fast paced San Francisco coffee shop during the launch of a number of innovative coffee products."
+    work.jobs.push(wornow);
+
+
+    var cof = new job();
+
+    cof.position = "Docent";
+    cof.employer = "Conservatory of Flowers";
+    cof.location = "San Francisco";
+    cof.years = "4 years";
+    cof.description = "Am a Jungle Guide for 2nd to 5th grade field tripts to the " +
+    "Conservatory of Flowers, which is a historical green house in Golden Gate Park.";
+
+    cof.addtowork();
+
+    console.log(cof)
+  },
+  job: new job(),
+
+
 }
+work.init();
 
-worknow = {};
-worknow.position = "Volunteer Supervisor";
-
-worknow.employer = "San Francisco Bicycle Coalition";
-
-worknow.location = "San Francisco";
-
-worknow.years = '3.5 years';
-
-worknow.description = "Oversee field operations for the bicycle valet service while overseeing volunteers and coordinating operations at a wide variety of sites.";
-
-console.table(worknow)
-
-
-work.jobs.push(worknow);
-var wornow = {};
-wornow.position = "Barista";
-wornow.employer = "It's a Grind";
-wornow.location = "San Francisco";
-wornow.years = "5 years";
-wornow.description = "Provide customer service in a fast paced San Francisco coffee shop during the launch of a number of innovative coffee products."
-work.jobs.push(wornow);
 
 console.log('now work is', work)
 
@@ -57,18 +78,7 @@ function job(){
   }
 }
 
-var cof = new job();
 
-cof.position = "Docent";
-cof.employer = "Conservatory of Flowers";
-cof.location = "San Francisco";
-cof.years = "4 years";
-cof.description = "Am a Jungle Guide for 2nd to 5th grade field tripts to the " +
-"Conservatory of Flowers, which is a historical green house in Golden Gate Park.";
-
-cof.addtowork();
-
-console.log(cof)
 
 
 var education = {"schools":[],
@@ -76,7 +86,19 @@ var education = {"schools":[],
               "title":"JavaScript Basics",
               "school":"Udacity",
               "url":"https://www.udacity.com/course/ud804",
-              "dates":"April 2014"}
+              "dates":"April 2015"},
+              {
+                "title":"Data Visualization and D3.js",
+                "school":"Udacity",
+                "dates":"Jan - Feb 2015",
+                "url":"https://www.udacity.com/course/data-visualization-and-d3js--ud507"
+              },
+              {
+                "title":"How to use Git and GitHub",
+                "school":"Udacity",
+                "dates":"Jan 2015",
+                "url":"https://www.udacity.com/course/how-to-use-git-and-github--ud775"
+              }
             ]};
 
 education.schools.push({"location":"San Francisco, CA"})
@@ -93,7 +115,8 @@ education.schools.push({"name":"University of Wisconsin",
 
 education.schools[0]["major"]  = "Geographic Information Systems"
 
-
+// online course gets title, school, url and dates
+//var introcs = new onlineCourse("Computer");
 
 var projects = {projects:[{
   "title":"California State Congresses Maps",
@@ -112,18 +135,35 @@ var projects = {projects:[{
     "images":[
 
     ]
-  }]}
+  }],
+  init:function(){
+    var baseball = new Project
 
-  var baseball = new Project
+    baseball.title = "MLB World Series Column Chart";
+    baseball.dates = "October 2014";
+    baseball.description = "Major League Baseball World series total wins column chart with map showing current franchise locations.";
+    baseball.url = "http://mpmckenna8.github.io/chartMap/index.html";
+    baseball.images = [];
 
-  baseball.title = "MLB World Series Column Chart";
-  baseball.dates = "October 2014";
-  baseball.description = "Major League Baseball World series total wins column chart with map showing current franchise locations.";
-  baseball.url = "http://mpmckenna8.github.io/chartMap/index.html";
-  baseball.images = [];
+    console.log(baseball)
 
-  console.log(baseball)
-projects.projects.push(baseball);
+    projects.projects.push(baseball);
+
+
+  }}
+
+  var badgers = new Project
+
+  badgers.title = "Badgers Twitter Map";
+  badgers.dates = "March 2015";
+  badgers.description = "A map that utilizes the Twitter Streaming API and Mapbox Tiles to show where people are tweeting about the badgers.";
+  badgers.url = "https://polar-ridge-9307.herokuapp.com/";
+  badgers.images = [];
+
+  projects.projects.push(badgers);
+
+projects.init();
+
 
 // data ended not to do a octo
 
@@ -131,118 +171,171 @@ var octo = {
   getbio: function(){
     return bio
   },
-  
+  getproj: function(){
+    return projects;
+  },
+  getschools: function(){
+    return education.schools;
+  },
+  getcourses: function(){
+    return education.onlineCourses;
+  }
 
 }
+
+// now doing the view stuff, i guess I'll try and put all the stuff that interacts with the view in heresies
 
 var view = {
+  init:function(){
+    this.work.init();
+    view.bio.init();
+    view.projects.init();
+    view.education.init();
 
-}
-projects.display = function(){
-  console.log(this)
-  var projdiv = $("#projects")
-  for(i in this.projects){
-    console.log(this.projects[i])
+  },
+  projects:{init:function(){
+    var projectos = octo.getproj().projects;
 
-    projdiv.append(HTMLprojectStart);
+    console.log(projectos)
+
+    var projdiv = $("#projects")
+
+    for(i in projectos){
 
 
+      projdiv.append(HTMLprojectStart);
 
-    $(".project-entry:last").append(HTMLprojectTitle.replace("%data%", this.projects[i].title).replace("#", this.projects[i].url)
-    + HTMLprojectDates.replace("%data%", this.projects[i].dates)
-     + HTMLprojectDescription.replace("%data%", this.projects[i].description) )
 
-     if(projects.projects[i].images.length > 0){
-       for (image in projects.projects[i].images){
-         var formimage = HTMLprojectImage.replace("%data%", projects.projects[i].images[image].url);
-         $(".project-entry:last").append(formimage)
+      $(".project-entry:last").append(HTMLprojectTitle.replace("%data%", projectos[i].title).replace("#", projectos[i].url)
+      + HTMLprojectDates.replace("%data%", projectos[i].dates)
+       + HTMLprojectDescription.replace("%data%", projectos[i].description) )
+
+       if(projectos[i].images.length > 0){
+         for (image in projectos[i].images){
+           var formimage = HTMLprojectImage.replace("%data%", projectos[i].images[image].url);
+           $(".project-entry:last").append(formimage)
+         }
        }
-     }
 
 
+    }
+  }},
+  bio:{
+    init:function(){
+
+      // make the bio show up
+
+      var formatName = HTMLheaderName.replace("%data%", bio.name);
+      console.log(formatName);
+
+
+      var formatRole = HTMLheaderRole.replace("%data%", bio.role);
+
+      $("#header").prepend(formatRole);
+      $("#header").prepend([formatName]);
+
+      //var formatCon = HTMLcontactGeneric.replace("%data%", bio.contacts.mobile).replace("%contact%", "Best to contact by:");
+
+      var forMob = HTMLmobile.replace("%data%", bio.contacts.mobile);
+
+      var formatemail = HTMLemail.replace("%data%", bio.contacts.email);
+
+      var formtwit = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+
+
+      var formskills = [];
+      for(i in bio.skills){
+        console.log(i)
+        formskills.push(HTMLskills.replace("%data%", bio.skills[i]))
+
+      }
+
+      var formskillsStart = HTMLskillsStart;
+
+      $("#topContacts")
+      .append(formatemail).append(forMob).append(formtwit)  //.append(formatCon)
+
+      if(bio.skills.length > 0 ){
+
+      $("#header").append(formskillsStart)//.append(formskills);
+        $("#skills").append(formskills)
+      }
+
+      console.log(formskills)
+
+      var formpic = HTMLbioPic.replace("%data%", bio.pic);
+
+      var formwel = HTMLwelcomeMsg.replace("%data%", bio.welcome)
+
+      $("#header").append(formpic).append(formwel)
+
+    }
+  },
+  work:{
+    init:function(){
+      for(i in work.jobs){
+        displayWork(work, i)
+
+      }
+    }
+  },
+  education:{
+    init:function(){
+      var schools = octo.getschools();
+
+      var edudiv = $("#education");
+      for( ble in schools){
+        edudiv.append(HTMLschoolStart);
+
+        var hoo = schools[ble];
+        var name = HTMLschoolName.replace("%data%", hoo.name);
+        var degree = HTMLschoolDegree.replace("%data%", hoo.degree);
+        var dates = HTMLschoolDates.replace("%data%", hoo.years);
+        var loca = HTMLschoolLocation.replace("%data%", hoo.location );
+        var major = HTMLschoolMajor.replace("%data%", hoo.major);
+        var allofit = name + degree + dates + loca + major;
+        $(".education-entry:last").append(allofit);
+      }
+
+      this.onlineClass();
+
+
+    },
+    onlineClass:function(){
+      var classes = octo.getcourses();
+
+      console.log(classes);
+      var ediv = $("#education");
+
+      ediv.append(HTMLonlineClasses);
+
+      for (cla in classes){
+        console.log(classes[cla]);
+
+        ediv.append(HTMLschoolStart);
+
+        // We have title, school, dates and url in each online class data thing
+        var entry = HTMLonlineTitle.replace("%data%", classes[cla].title).replace("#", classes[cla].url)
+                  + HTMLonlineSchool.replace("%data%", classes[cla].school)
+                  + HTMLonlineDates.replace("%data%", classes[cla].dates)
+                  + HTMLonlineURL.replace("%data%", classes[cla].url);
+                  console.log(entry);
+
+                 $(".education-entry:last")
+                .append(entry);
+      }
+    }
   }
-}
-projects.display();
-
-
-var formatName = HTMLheaderName.replace("%data%", bio.name);
-console.log(formatName);
-
-
-var formatRole = HTMLheaderRole.replace("%data%", bio.role);
-
-$("#header").prepend(formatRole);
-$("#header").prepend([formatName]);
-
-//var formatCon = HTMLcontactGeneric.replace("%data%", bio.contacts.mobile).replace("%contact%", "Best to contact by:");
-
-var forMob = HTMLmobile.replace("%data%", bio.contacts.mobile);
-
-var formatemail = HTMLemail.replace("%data%", bio.contacts.email);
-
-var formtwit = HTMLtwitter.replace("%data%", bio.contacts.twitter);
-
-
-var formskills = [];
-for(i in bio.skills){
-  console.log(i)
-  formskills.push(HTMLskills.replace("%data%", bio.skills[i]))
 
 }
 
-var formskillsStart = HTMLskillsStart;
-
-$("#topContacts")
-.append(formatemail).append(forMob).append(formtwit)  //.append(formatCon)
-
-if(bio.skills.length > 0 ){
-
-$("#header").append(formskillsStart)//.append(formskills);
-  $("#skills").append(formskills)
+view.init();
 
 
-}
-
-console.log(formskills)
-
-var formpic = HTMLbioPic.replace("%data%", bio.pic);
-
-var formwel = HTMLwelcomeMsg.replace("%data%", bio.welcome)
-
-$("#header").append(formpic).append(formwel)
-
-
-var soap;
-
-for(i in work.jobs){
-  displayWork(work, i)
-
-}
+// all the below are
 
 //$("#workExperience").append(formWorkLen).append(formWorkEm).append(formworkTit).append(formworkDate).append(formWorkPlace).append(formWorkDesc)
-education.init = function(){
-  console.log(this)
 
-  var edudiv = $("#education");
-  for( ble in this.schools){
-    edudiv.append(HTMLschoolStart);
-
-    var hoo = this.schools[ble];
-    var name = HTMLschoolName.replace("%data%", hoo.name);
-    var degree = HTMLschoolDegree.replace("%data%", hoo.degree);
-    var dates = HTMLschoolDates.replace("%data%", hoo.years);
-    var loca = HTMLschoolLocation.replace("%data%", hoo.location );
-    var major = HTMLschoolMajor.replace("%data%", hoo.major);
-    var allofit = name + degree + dates + loca + major;
-    $(".education-entry:last").append(allofit)
-  }
-
-}
-
-education.init();
-education.displayone = function(){
-
-}
 
 
 function displayWork(work,i){
@@ -275,7 +368,17 @@ function inName(name){
 function Project(){
   this.title;
   this.dates;
+  this.url;
+  this.images = [];
 
 }
 
 $("#mapDiv").append(googleMap)
+
+
+function onlineCourse(title, school, dates, url){
+  this.title = title;
+  this.school = school;
+  this.dates = dates;
+  this.url = url;
+}
